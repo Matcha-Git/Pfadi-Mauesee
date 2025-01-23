6b517fa5ebb34fb6684e13e2032fc9c25804d8e0
Matcha-Git <187616854+Matcha-Git@users.noreply.github.com>
2025-01-23 11:49:07 +0100

    wobsite is done (for now)

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
-$file = 'out.txt';
+// Überprüfen, ob das Formular abgesendet wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sammeln der Formulardaten
+    $vorname = $_POST['Vor-/Nachname'];
+    $alter = $_POST['Alter'];
+    $stufe = $_POST['select1'];
+    $telefon = $_POST['phone'];
+    $inhaber = $_POST['Inhabertel'];
+    $mitteilung = $_POST['Mitteilung'];
 
-$message = $_POST['message'] ?? 'No message';
+    // Formatieren der Daten für die Textdatei
+    $data = "Vor-/Nachname: $vorname\n";
+    $data .= "Alter: $alter\n";
+    $data .= "Schnupper Stufe: $stufe\n";
+    $data .= "Telefonnummer: $telefon\n";
+    $data .= "Inhaber der Telefonnummer: $inhaber\n";
+    $data .= "Mitteilung: $mitteilung\n\n";
 
-$message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+    // Öffnen der Datei und Anhängen der Formulardaten
+    $file = fopen("form_data.txt", "a");
 
-$entry = "Message: $message\n";
-
-file_put_contents($file, $entry, FILE_APPEND | LOCK_EX);
+    // Daten in die Datei schreiben
+    if ($file) {
+        fwrite($file, $data);
+        fclose($file);
+        echo "Daten erfolgreich gespeichert!";
+    } else {
+        echo "Fehler beim Speichern der Daten.";
+    }
+}
 ?>
